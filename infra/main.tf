7f22b6ea9f1e455c6ea97efbf172738c9bafd15c
guilhermefrazao <guilhermefrazao@discente.ufg.br>
2025-11-21 07:49:38 -0300

    fix: terraform cluster update

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,42 @@ import {
   id = "projects/pdm-2025-creditos/serviceAccounts/pdm-2025-creditos@pdm-2025-creditos.iam.gserviceaccount.com"
 }
 
+terraform {
+  required_providers {
+    google     = { source = "hashicorp/google", version = ">= 4.0" }
+    helm       = { source = "hashicorp/helm", version = ">= 2.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.0" }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "cluster_airflow" {
+  name     = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
+}
+
+
+provider "helm" {
+  kubernetes = {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
 
 resource "google_project_service" "gcp_services" {
   for_each = toset(var.gcp_service_list)
