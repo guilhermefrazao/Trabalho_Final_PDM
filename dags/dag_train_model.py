60c553506983deaf005d0c5a4c9289c5dda05c5b
guilhermefrazao <guilhermefrazao@discente.ufg.br>
2025-11-22 12:05:56 -0300

    feat: improving dag

--- dags/dag_train_model.py
+++ dags/dag_train_model.py
@@ -1,7 +1,3 @@
-# ===============================================
-# Exemplo completo de um DAG no Apache Airflow
-# ===============================================
-
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -10,40 +6,55 @@ import logging
 from datetime import datetime
 
 
-log = logging.getLogger("meu_app")
-
 MLFLOW_URI = "http://mlflow-service.default.svc.cluster.local:5000"
 
 def treinar_modelo():
+    import logging
     import mlflow
     import mlflow.sklearn
     from sklearn.linear_model import LinearRegression
     import numpy as np
 
+    logger = logging.getLogger("airflow.task")
+    
+    logger.info("Iniciando a função de treinamento do modelo.")
+
     mlflow.set_tracking_uri(MLFLOW_URI)
     mlflow.set_experiment("exemplo-registro-modelo")
     
+    logger.info("Preparando dados de treino...")
     X = np.array([[1], [2], [3], [4]])
     y = np.array([2, 4, 6, 8])
 
+    logger.info("Instanciando e treinando LinearRegression...")
     model = LinearRegression()
     model.fit(X, y)
+    logger.info("Treinamento concluído.")
 
     with mlflow.start_run() as run:
+        run_id = run.info.run_id
+        logger.info(f"Iniciando MLflow Run ID: {run_id}")
+
         mlflow.sklearn.log_model(model, artifact_path="modelo")
+        logger.info("Modelo logado no MLflow.")
+
         mlflow.log_metric("rmse", 0.0)
+        logger.info("Métricas logadas.")
         
-        model_uri = f"runs:/{run.info.run_id}/modelo"
+        model_uri = f"runs:/{run_id}/modelo"
         
+        logger.info(f"Registrando modelo com URI: {model_uri}")
         mv = mlflow.register_model(model_uri=model_uri, name="modelo_linear_teste")
         
         client = mlflow.tracking.MlflowClient()
+        logger.info(f"Transicionando versão {mv.version} para Production...")
+        
         client.transition_model_version_stage(
             name="modelo_linear_teste",
             version=mv.version,
             stage="Production"
         )
-        print(f"Modelo versão {mv.version} promovido para Production")
+        logger.info(f"SUCESSO: Modelo versão {mv.version} promovido para Production.")
 
 
 default_args = {
@@ -67,19 +78,17 @@ with DAG(
     tags=["exemplo", "tutorial"],            # tags para filtro no UI
 ) as dag:
 
-    # Tarefa 1: apenas imprime algo no console
+
     tarefa_inicial = BashOperator(
         task_id="inicio",
         bash_command="echo 'Iniciando treinamento do modelo...'"
     )  
 
-    # Tarefa 2: executa uma função Python
     treinamento = PythonOperator(
         task_id="execute_training_with_mlflow",
         python_callable = treinar_modelo     
     )
 
-    # Tarefa 3: simula finalização
     tarefa_final = BashOperator(
         task_id="fim",
         bash_command="echo 'Finalizado treinamento do modelo com sucesso!'"
